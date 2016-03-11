3b3e666ec8ad1e1e8e049110f8851e05ac027351
amorimoto <morimoto@team-lab.com>
2016-03-11 16:12:22 +0900

    画像の書き出しをしてみる

---
+++ Electron/README.md
@@ -0,0 +1,8 @@
+## Electron + babel + es6
+
+#### 導入
+http://qiita.com/inuscript/items/a3822167604e5ad6c19b
+
+#### jQuery
+http://qiita.com/pirosikick/items/72d11a8578c5c3327069
+
--- Electron/app/client/main.jsx
+++ Electron/app/client/main.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import fs from 'fs'
+import request from 'request'
 
 class Foo extends React.Component {
+
   render() {
-    return <div>This is React component</div>
+    return (
+      <input type="button" value="download" onClick={this.handleClick}/>
+    )
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    request(
+      {method: 'GET', url: 'http://nekogazou.com/wp-content/uploads/2015/08/55270573c8c1630b9b2901f4da9989c7.jpg', encoding: null}, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+          fs.writeFileSync('a.png', body, 'binary');
+        }
+      }
+    );
+  }
+
+  readFile() {
+    // ファイルの非同期読み込み
+    fs.readFile(__dirname + '/hoge.txt', 'utf8', (err, text) => {
+      console.log('text file!');
+      console.log(text);
+      console.log('error!?');
+      console.log(err);
+    });
   }
 }
 
-var container = document.querySelector("#container")
-
-React.render(<Foo />, container)
+ReactDOM.render(<Foo />, document.getElementById('container'));
